36f440382cb1d31d467962091ebfe4b641f91aa2
thenam2kx <thenam2kx@gmail.com>
2025-02-16 00:43:41 +0700

    crud module product

--- src/modules/products/schemas/product.schema.ts
+++ src/modules/products/schemas/product.schema.ts
@@ -1,1 +1,75 @@
-export class Product {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Category } from 'src/modules/categories/schemas/category.schema';
+
+@Schema({ timestamps: true, versionKey: false })
+export class Product {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  slug: string;
+
+  @Prop()
+  description: string;
+
+  @Prop({ default: [] })
+  images: string[];
+
+  @Prop({ required: true })
+  price: number;
+
+  @Prop({ required: true, min: 0 })
+  stock: number;
+
+  @Prop()
+  dateManufacture: Date;
+
+  @Prop()
+  expiryDate: Date;
+
+  @Prop()
+  weight: number;
+
+  @Prop({
+    enum: ['KG', 'CON', 'HỘP'],
+    default: 'KG',
+  })
+  unit: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: Category.name })
+  category: mongoose.Schema.Types.ObjectId;
+
+  @Prop({ default: true })
+  isAvailable: boolean;
+
+  @Prop({ type: Object })
+  createdBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object })
+  updatedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object })
+  deletedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  updatedAt: Date;
+
+  @Prop()
+  deletedAt: Date;
+}
+
+export type ProductDocument = HydratedDocument<Product>;
+export const ProductSchema = SchemaFactory.createForClass(Product);
