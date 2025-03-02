0756d322784d904e739f76f2df6b3f713f4aff0e
thenam2kx <thenam2kx@gmail.com>
2025-03-03 01:16:21 +0700

    fix response update role

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ async function bootstrap() {
 
   // Config CORS
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'http://localhost:4173'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     credentials: true,
